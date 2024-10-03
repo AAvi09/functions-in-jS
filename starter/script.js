2ed71cbb93ada3e70996cf508f8d73fcb4fe88aa
Avinash sharma <sharmaavi2196@gmail.com>
2024-10-04 02:55:19 +0530

    poll method and display method created

--- starter/script.js
+++ starter/script.js
@@ -115,3 +115,32 @@ AirIndia.buyPlanes = function () {
 };
 
 AirIndia.buyPlanes();
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+
+  registerAnswer: function () {
+    const answer = Number(
+      prompt(`${this.question} \n${this.options.join(`\n`)}\nwrite your option`)
+    );
+    console.log(answer);
+
+    typeof answer === Number &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+  },
+
+  displayResult(type = 'array') {
+    if ((type = 'array')) {
+      console.log(`${this.answers}`);
+    } else if ((type = 'string')) {
+      console.log(`poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerAnswer.bind(poll));
